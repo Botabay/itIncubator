7ac2795925a3c521fa40d6131c9a8f929e2082ff
Botabay <tohgl49@gmail.com>
2023-04-07 10:34:17 +0600

    did some actions for a make the directory to be main of TODOLIST-PROJECT for long time

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
+import { Todolist } from './components/Todolist';
 
-function App() {
-
-    const tasks1 = [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
+export const App=()=> {
+    const titleValue='asdfadfadfasdfkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk';
+    const tasks1= [
+        {taskId: 1, title: "HTML&CSS1", isDone: true},
+        {taskId: 2, title: "JS1", isDone: true},
+        {taskId: 3, title: "TS", isDone: false}
+    ];
+    const tasks2= [
+        {taskId: 1, title: "HTML&CSS2", isDone: true},
+        {taskId: 2, title: "JS2", isDone: true},
+        {taskId: 3, title: "TS2", isDone: false},
+        {taskId: 4, title: "PHP", isDone: true}
     ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
-    ]
-
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks1} />
-            <Todolist title="Songs" tasks={tasks2} />
+            <Todolist title={'What to learn'} body={'this is body'} tasks={tasks1}/>
+            <Todolist title={'What to remember'} tasks={tasks2}/>
+            {/* <Todolist title={titleValue}/> */}
         </div>
     );
 }
-
-export default App;
